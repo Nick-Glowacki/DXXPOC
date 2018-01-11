3384463aeb6d6040bf6042f11a1c4fce8c61bf82
Comet-Development <nick.glowacki@ibm.com>
2018-01-11 13:39:52 -0500

    Woohoo, hopefully a good demo that was clear

---
+++ buildtemp.sh
@@ -0,0 +1,3 @@
+apic auth:login --server us.apiconnect.ibmcloud.com
+apic config:set catalog=apic-catalog://us.apiconnect.ibmcloud.com/orgs/59e90d130cf2852274d1dd34/catalogs/sb
+apic publish temp_1.0.0_1.yaml
